515dcb36874ba548f6f605ecc4474fb0d8daeea0
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-06-05 19:25:11 +0530

    Add new new method, class and test case for StateCodeCsv

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -9,6 +9,7 @@ public class CensusAnalyserTest {
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
     private static final String WRONG_EXTENSION_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+    private static final String INDIA_STATE_CSV_FILE_PATH = "./src/main/resources/IndiaStateCode.csv";
 
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
@@ -71,5 +72,12 @@ public class CensusAnalyserTest {
         }
     }
 
-
+    @Test
+    public void givenIndianStateCSV_ShouldReturnExactCount() {
+        try {
+        CensusAnalyser censusAnalyser = new CensusAnalyser();
+        int numOfStateCode = censusAnalyser.loadIndianStateCode(INDIA_STATE_CSV_FILE_PATH);
+        Assert.assertEquals(37,numOfStateCode);
+        } catch (CensusAnalyserException e) { }
+    }
 }
