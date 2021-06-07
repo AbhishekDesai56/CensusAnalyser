2d65418d4f5b74394f24106e0831fb846a721526
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-06-07 17:47:57 +0530

    Refactor Code to support in place of iterator

--- src/main/java/censusanalyser/CensusAnalyserException.java
+++ src/main/java/censusanalyser/CensusAnalyserException.java
@@ -8,6 +8,7 @@ public class CensusAnalyserException extends Exception {
 
     ExceptionType type;
 
+
     public CensusAnalyserException(String message, String name) {
         super(message);
         this.type = ExceptionType.valueOf(name);
